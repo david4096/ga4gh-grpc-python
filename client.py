088733fd388b8acd714d429b2857ba3e25507f7c
David Steinberg <david@resium.com>
2016-02-04 18:34:33 -0800

    Added stubs for the rest

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ def run():
   channel = implementations.insecure_channel('localhost', 50051)
   stub = read_service_pb2.beta_create_ReadService_stub(channel)
   response = stub.GetReadGroupSet(read_service_pb2.GetReadGroupSetRequest(read_group_set_id='you'), _TIMEOUT_SECONDS)
+  response = stub.GetReadGroupSet(read_service_pb2.SearchReadsRequest(read_group_ids=['a'], reference_id="1", start=0, end=100))
   print response
 
 
--- server.py
+++ server.py
@@ -3,21 +3,78 @@ import time
 import read_service_pb2
 import reads_pb2
 
-
-class Server(read_service_pb2.BetaReadServiceServicer):
+class ReadService(read_service_pb2.BetaReadServiceServicer):
 
   def SearchReadGroupSets(self, request, context):
-    print(read_service_pb2.SearchReadGroupSetsResponse)
-    return read_service_pb2.SearchReadGroupSetsResponse
+    return read_service_pb2.SearchReadGroupSetsResponse()
 
   def GetReadGroupSet(self, request, context):
-    print(request)
-
     return reads_pb2.ReadGroupSet(id=request.read_group_set_id)
 
   def SearchReads(self, request, context):
-    return True
+    return read_service_pb2.SearchReadsResponse()
 
+
+import variant_service_pb2
+import variants_pb2
+
+class VariantService(variant_service_pb2.BetaVariantServiceServicer):
+
+  def SearchVariantSets(self, request, context):
+    return variant_service_pb2.SearchVariantSetsResponse()
+
+  def SearchCallSets(self, request, context):
+    return variant_service_pb2.SearchCallSetsResponse()
+
+  def SearchVariants(self, request, context):
+    return variant_service_pb2.SearchVariantsResponse()
+
+  def GetCallSet(self, request, context):
+    return variants_pb2.CallSet()
+
+  def GetVariant(self, request, context):
+    return variants_pb2.Variant()
+
+  def GetVariantSet(self, request, context):
+    return variants_pb2.VariantSet()
+
+
+import metadata_service_pb2
+import metadata_pb2
+
+class MetadataService(metadata_service_pb2.BetaMetadataServiceServicer):
+
+  def SearchDatasets(self, request, context):
+    return metadata_service_pb2.SearchDatasetsResponse()
+
+  def GetDataset(self, request, context):
+    return metadata_pb2.Dataset()
+
+
+import reference_service_pb2
+import references_pb2
+
+class ReferenceService(reference_service_pb2.BetaReferenceServiceServicer):
+
+  def SearchReferences(self, request, context):
+    return reference_service_pb2.SearchReferencesResponse()
+
+  def SearchReferenceSets(self, request, context):
+    return reference_service_pb2.SearchReferenceSetsResponse()
+
+  def GetReferenceSet(self, request, context):
+    return references_pb2.ReferenceSet()
+
+  def GetReference(self, request, context):
+    return references_pb2.Reference()
+
+  def ListReferenceBases(self, request, context):
+    return reference_service_pb2.ListReferenceBasesResponse()
+
+
+class Server(VariantService, ReadService, MetadataService, ReferenceService):
+  def __init__(self):
+    return None
 
 def serve():
   server = read_service_pb2.beta_create_ReadService_server(Server())
